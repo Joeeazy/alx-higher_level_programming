f4bb52373791efddc48c1bd43f12064cf865b222
Joeeazy <joeeazy34@gmail.com>
2024-01-08 23:57:01 +0300

    First constant, first print

---
+++ 0x12-javascript-warm_up/1-multi_languages.js
@@ -0,0 +1,11 @@
+let firstLine;
+let secondLine;
+let thirdLine;
+
+firstLine = "C is fun";
+secondLine = "Python is cool";
+thirdLine = "JavaScript is amaizing";
+
+console.log(firstLine);
+console.log(secondLine);
+console.log(thirdLine);
