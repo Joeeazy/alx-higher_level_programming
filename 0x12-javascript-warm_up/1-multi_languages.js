d05675def95bb4c23478ef76fcb15ae9a0535163
Joeeazy <joeeazy34@gmail.com>
2024-01-08 19:14:36 -0800

    3 languages

--- 0x12-javascript-warm_up/1-multi_languages.js
+++ 0x12-javascript-warm_up/1-multi_languages.js
@@ -1,9 +1,5 @@
 #!/usr/bin/node
 
-let firstLine = "C is fun";
-let secondLine = "Python is cool";
-let thirdLine = "JavaScript is amaizing";
-
-console.log(firstLine);
-console.log(secondLine);
-console.log(thirdLine);
+console.log("C is fun");
+console.log(“Python is cool”);
+console.log(“JavaScript is amazing”);
