b021f3a7f7185d2eaf9ab1406d313f5d3305c879
Joeeazy <joeeazy34@gmail.com>
2024-02-28 19:32:51 +0300

    No Jquery

--- 0x15-javascript-web_jquery/0-script.js
+++ 0x15-javascript-web_jquery/0-script.js
@@ -1,8 +1,3 @@
 //JavaScript script that updates the text color of the <header> element to red (#FF0000)
 
-document.addEventListener('DOMContentLoaded', function() {
-    var header = document.querySelector('header');
-    if (header) {
-        header.style.color = '#FF0000';
-    }
-});
+document.querySelector('header').style.color = '#FF0000';
